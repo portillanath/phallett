60b3a665d90373eebb55ad432a08af99dcb90c5b
portillanath <na.portilla10@uniandes.edu.co>
2024-10-15 07:22:54 -0500

    all changes set up

---
+++ src/06.Graphing.py
@@ -0,0 +1,167 @@
+import os
+import re
+import glob
+import pandas as pd
+import numpy as np
+import sys
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+from pathlib import Path
+
+# Set working directory
+current_dir = Path.cwd()
+parent_dir = current_dir.parent
+workdir = os.path.expanduser((Path(parent_dir) / "phallett" / "test" / "Metrics_Results"))
+subdirectories = [os.path.join(workdir, name) for name in os.listdir(workdir) if os.path.isdir(os.path.join(workdir, name))]
+
+import argparse
+
+# Create the argument parser
+parser = argparse.ArgumentParser(description='Process some arguments.')
+
+# Add arguments
+parser.add_argument('-mx', type=str, help='The mx argument')
+parser.add_argument('-kmersx', type=str, help='The kmersx argument')
+parser.add_argument('-my', type=str, help='The my argument')
+parser.add_argument('-kmersy', type=str, help='The kmersy argument')
+
+# Parse the arguments
+args = parser.parse_args()
+
+# Access the arguments
+print(f'mx: {args.mx}, kmersx: {args.kmersx}, my: {args.my}, kmersy: {args.kmersy}')
+
+mx = args.mx
+my = args.my
+kmersx = [int(k) for k in args.kmersx.split(",")]
+kmersy = [int(k) for k in args.kmersy.split(",")]
+
+# Cases for metrics selected on Y
+tool_my = []
+tool_mx = []
+
+if my == "mash":
+    tool_my = ["mash", "sourmash"]
+elif my == "ani":
+    tool_my = ["fastani", "skani"]
+elif my == "aai":
+    tool_my = ["comparem"]
+elif my == "viridic":
+    tool_my = ["viridic"]
+elif my == "vcontact2":
+    tool_my = ["vcontact2"]
+
+if mx == "mash":
+    tool_mx = ["mash", "sourmash"]
+elif mx == "ani":
+    tool_mx = ["fastani", "skani"]
+elif mx == "aai":
+    tool_mx = ["comparem"]
+elif mx == "viridic":
+    tool_mx = ["viridic"]
+elif mx == "vcontact2":
+    tool_mx = ["vcontact2"]
+
+metrics = [mx, my]
+
+ani_data = pd.DataFrame()
+mash_data = pd.DataFrame()
+viridic_data = pd.DataFrame()
+mx_data = pd.DataFrame()
+my_data = pd.DataFrame()
+df_subset_mx = pd.DataFrame()
+df_subset_my = pd.DataFrame()
+kmer_values_mx = []
+kmer_values_my = []
+
+for m in metrics:
+    if m == mx:
+        kmers = kmersx
+        tool_mx = tool_mx
+    if m == my:
+        kmers = kmersy
+        tool_my = tool_my
+
+for genus in subdirectories:
+    os.chdir(genus)
+    genus_name = os.path.basename(genus)
+    genus_dir = os.path.join(workdir, genus_name)
+    summaries = glob.glob(os.path.join(genus_dir, "*metrics*.csv"))
+
+    if mx in metrics:
+        mx_files = [s for s in summaries if f"{mx}_metrics_" in s]
+        if len(mx_files) > 0:
+            mx_data = pd.read_csv(mx_files[0])
+        else:
+            print(f"No {mx} data for {genus_name}")
+    if my in metrics:
+        my_files = [s for s in summaries if f"{my}_metrics_" in s]
+        if len(my_files) > 0:
+            my_data = pd.read_csv(my_files[0])
+        else:
+            print(f"No {my} data for {genus_name}")
+
+    # Assuming merged_df is your DataFrame
+    for algorithm_mx in tool_mx:
+        for algorithm_my in tool_my:
+            # Subset the data for the current combination
+            df_subset_mx = mx_data[mx_data[f"algorithm_{mx}"] == algorithm_mx]
+            kmer_values_mx = sorted(pd.unique(df_subset_mx[f"kmer_{mx}"]))
+            df_subset_my = my_data[my_data[f"algorithm_{my}"] == algorithm_my]
+            kmer_values_my = sorted(pd.unique(df_subset_my[f"kmer_{my}"]))
+
+            # Create a scatterplot for each kmer combination
+            fig, axes = plt.subplots(len(kmer_values_mx), len(kmer_values_my), figsize=(12, 8), sharex=True)
+            axes = axes.reshape(len(kmer_values_mx), len(kmer_values_my))
+            kmer_values_mx.sort()
+            kmer_values_my.sort(reverse=True)
+
+            # Iterate through kmer values
+            for j, my_kmer in enumerate(kmer_values_my):
+                for i, mx_kmer in enumerate(kmer_values_mx):
+                    # Subset the data for the current kmer_mx and kmer_my
+                    df_subset_mx_kmer = df_subset_mx[df_subset_mx[f"kmer_{mx}"] == mx_kmer]
+                    df_subset_my_kmer = df_subset_my[df_subset_my[f"kmer_{my}"] == my_kmer]
+
+                    # Check if there are any data points for the current kmer combination
+                    merged_df = pd.merge(df_subset_mx_kmer, df_subset_my_kmer, on=["GenomeA", "GenomeB"], how="outer")
+                    merged_df.sort_values(by=[f"{mx}_distance", f"{my}_distance"], inplace=True)
+                    merged_df = merged_df.dropna()
+
+                    # Check if merged_df is not empty before plotting
+                    if not merged_df.empty:
+                        # Initialize colors based on mx_distance
+                        colors = []
+                        for x_value in merged_df[f"{mx}_distance"]:
+                            if x_value <= 88.00:
+                                colors.append('red')
+                            elif 88.00 < x_value <= 94.00:
+                                colors.append('orange')
+                            else:  # x_value >= 94.00
+                                colors.append('yellow')
+
+                        # Create a scatterplot for the current kmer_mx and kmer_my
+                        ax = axes[i, j]
+                        ax.set_title(f"{algorithm_mx} kmer: {kmer_values_mx[i]}", fontsize=6)
+                        ax.tick_params(axis='both', which='major', labelsize=4)
+
+                        # Plot the scatter points
+                        scatter = ax.scatter(merged_df[f"{mx}_distance"], merged_df[f"{my}_distance"],
+                                             c=colors, alpha=0.5, s=0.5)
+
+                        # Set the y-label outside the plot area
+                        ax.set_ylabel(f"{algorithm_my} kmer: {kmer_values_my[j]}", rotation=90, ha='center', fontsize=6)
+                        ax.yaxis.set_label_coords(-0.15, 0.5)  # Adjust x coordinate to move label outside
+
+            # Create a custom legend
+            handles = [plt.Line2D([0], [0], marker='o', color='w', label='Family >=88.00', markerfacecolor='red', markersize=5),
+                       plt.Line2D([0], [0], marker='o', color='w', label='Genus 88.00-94.00', markerfacecolor='orange', markersize=5),
+                       plt.Line2D([0], [0], marker='o', color='w', label='Species >=94.00', markerfacecolor='yellow', markersize=5)]
+            fig.legend(handles=handles, loc='upper right', fontsize=8)
+
+            plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.1, wspace=0.2, hspace=0.2)
+
+            # Save the current figure to a PDF file if there are valid combinations
+            pdf_filename = f"{genus_name}_{algorithm_mx}_{algorithm_my}.pdf"
+            with PdfPages(pdf_filename) as pdf:
+                pdf.savefig(fig)
---
+++ src/06.Graphing.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#Author:Nathalia Portilla
+#name script:wraggling.sh
+#SBATCH -o Graphing_job.o%j
+
+kmersy=(7,9,11,12,13)
+kmersx=(12,11,10,9,8)
+my="mash"
+mx="ani"
+parent_dir=$(dirname "$PWD")
+
+while getopts "mx:kmersx:my:kmersy" option; do
+    case $option in
+        mx) #Handle the -mx flag with an argument
+        mx=${OPTARG}
+        ;;
+        kmersx) #Handle the -kmersx flag with an argument
+        kmersx=${OPTARG} 
+        ;;
+        my) #Handle the -my flag with an argument
+        my=${OPTARG}
+        ;;
+        kmersy) #Handle the -kmersy flag with an argumentcd
+        kmersy=${OPTARG}
+        ;;
+    esac
+done
+
+python3 "$parent_dir/phallett/src/05.Graphing.py" -mx "$mx" -kmersx "$kmersx" -my "$my" -kmersy "$kmersy" > Graphing.log 2>&1
+cat Graphing.log
