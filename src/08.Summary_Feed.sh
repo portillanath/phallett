6c169c03c58032a50799056d7fb9cf32d1595a94
Nathalia Andrea Portilla Rincón <67386652+portillanath@users.noreply.github.com>
2024-11-30 12:49:58 -0500

    Delete src/08.Summary_Feed.sh

--- src/08.Summary_Feed.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#Author:Nathalia Portilla
-#name script:summary_feed.sh
-#SBATCH -o Metrics_Wraggling_job.o%j
-
-parent_dir=$(dirname "$PWD")
-python3 "$parent_dir/src/08.Summary_Feed.py"
-
