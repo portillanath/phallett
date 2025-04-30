bcb78c348fe666c464daf0f622399bcbafb92f90
Nathalia Andrea Portilla Rincón <67386652+portillanath@users.noreply.github.com>
2025-04-30 13:19:03 -0500

    Update phallet.sh
    Update host version

--- phallet.sh
+++ phallet.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#Default argument values 
+
+# Default argument values 
 script_dir=$(dirname "$0")
 data_default=$(cat "$script_dir/test_genus.txt")
 module=""
@@ -7,99 +8,85 @@ kmersmash=(7 9 11 12 13)
 genus=""
 kmersani=(12 11 10 9 8)
 frag_lengths=(500)  
-kmersy=(15,17,20,21,24)
-kmersx=(12,11,10,9,8)
+kmersy=(15 17 20 21 24)
+kmersx=(12 11 10 9 8)
 my="mash"
 mx="ani"
 blastpor=0.75
 evalue=1e-5
-data_default=$(cat "$script_dir/test_genus.txt")
-file=$(cat "$script_dir/GCF_000836945.fasta")
+file="$script_dir/GCF_000836945.fasta"
 updatedb=false
+all_hosts_flag=""
 
-conda activate enviroments 
+# Activate conda environment
+conda activate enviroments
 
-#Parse arguments
-while getopts "d:m:g:ka:km:f:ky:kx:my:mx:fl:u:b:e" opt; do
+# Parse arguments
+while getopts "d:m:g:a:s:f:y:x:M:X:F:u:b:e:H" opt; do
   case $opt in
-    d)
-      data_default=$OPTARG
-      ;;
-    m)
-      module=$OPTARG
-      ;;
-    g)
-      genus=$OPTARG
-      ;;
-    ka)
-      kmersani=$OPTARG
-      ;;
-    km)
-      kmersmash=$OPTARG
-      ;;
-    f)
-      frag_lengths=$OPTARG
-      ;;
-    ky)
-      kmersy=$OPTARG
-      ;;
-    kx)
-      kmersx=$OPTARG
-      ;;
-    my)
-      my=$OPTARG
-      ;;
-    mx)
-      mx=$OPTARG
-      ;;
-    fl)
-      file=$OPTARG
-      ;;
-    u)  
-      updatedb=$OPTARG
-      ;;
-    b)
-      blastpor=$OPTARG
-      ;;
-    e)
-      evalue=$OPTARG
-      ;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2
-      ;;
+    d) data_default="$OPTARG" ;;
+    m) module="$OPTARG" ;;
+    g) genus="$OPTARG" ;;
+    a) kmersani=($OPTARG) ;;   # ANI k-mers
+    s) kmersmash=($OPTARG) ;;  # Mash k-mers
+    f) frag_lengths=($OPTARG) ;;
+    y) kmersy=($OPTARG) ;;
+    x) kmersx=($OPTARG) ;;
+    M) my="$OPTARG" ;;         # Metric y-axis (mash)
+    X) mx="$OPTARG" ;;         # Metric x-axis (ani)
+    F) file="$OPTARG" ;;
+    u) updatedb="$OPTARG" ;;
+    b) blastpor="$OPTARG" ;;
+    e) evalue="$OPTARG" ;;
+    H) all_hosts_flag="--all-hosts" ;;
+    \?) echo "Invalid option: -$OPTARG" >&2 ;;
   esac
 done
 
-#Run phallet steps as default 
+# Run phallett steps as default if no module is specified
 if [ -z "$module" ]; then
   bash "$script_dir/src/00.ICTV_Metadata_Resource_Resource.sh"
-  bash "$script_dir/src/01A.Taxa_Curation_Level.sh" $data_default
+  bash "$script_dir/src/01A.Taxa_Curation_Level.sh" "$data_default" $all_hosts_flag
   bash "$script_dir/src/02.Bargenome.sh"
-  bash "$script_dir/src/03.ANI_Metrics.sh" $kmersmash $genus
-  bash "$script_dir/src/04.Mash_Metrics.sh" $kmersani $genus $frag_lengths
-  bash "$script_dir/src/05.wraggling.sh" $kmersx $kmersy $my $mx
-  bash "$script_dir/src/06.Graphing.sh" $kmersx $kmersy $my $mx
+  bash "$script_dir/src/03.ANI_Metrics.sh" "${kmersmash[@]}" "$genus"
+  bash "$script_dir/src/04.Mash_Metrics.sh" "${kmersani[@]}" "$genus" "${frag_lengths[@]}"
+  bash "$script_dir/src/05.wraggling.sh" "${kmersx[@]}" "${kmersy[@]}" "$my" "$mx"
+  bash "$script_dir/src/06.Graphing.sh" "${kmersx[@]}" "${kmersy[@]}" "$my" "$mx"
 else
-  if [ "$module" == "ictv" ]; then
-    bash "$script_dir/src/00.ICTV_Metadata_Resource.sh"
-  elif [ "$module" == "taxa" ]; then
-    bash "$script_dir/src/01A.Taxa_Curation_Level.sh" $data_default
-  elif [ "$module" == "file" ]; then
-    bash "$script_dir/src/01B.Selecting_file.sh" $file $blastpor $evalue $updatedb
-  elif [ "$module" == "bargenome" ]; then
-    bash "$script_dir/src/02.Bargenome.sh"
-  elif [ "$module" == "ani" ]; then
-    bash "$script_dir/src/03.ANI_Metrics.sh" $kmersmash $genus
-  elif [ "$module" == "mash" ]; then
-    bash "$script_dir/src/04.Mash_Metrics.sh" $kmersani $genus $frag_lengths
-  elif [ "$module" == "wraggling" ]; then
-    bash "$script_dir/src/05.wraggling.sh" $kmersx $kmersy $my $mx
-  elif [ "$module" == "graphs" ]; then
-    bash "$script_dir/src/06.Graphing.sh" $kmersx $kmersy $my $mx
-  elif [ "$module"== "boxplot" ]; then
-    bash "$script_dir/src/07.Summary_Feed.sh" 
-  fi
+  case "$module" in
+    ictv)
+      bash "$script_dir/src/00.ICTV_Metadata_Resource.sh"
+      ;;
+    taxa)
+      bash "$script_dir/src/01A.Taxa_Curation_Level.sh" "$data_default" $all_hosts_flag
+      ;;
+    file)
+      bash "$script_dir/src/01B.Selecting_file.sh" "$file" "$blastpor" "$evalue" "$updatedb"
+      ;;
+    bargenome)
+      bash "$script_dir/src/02.Bargenome.sh"
+      ;;
+    ani)
+      bash "$script_dir/src/03.ANI_Metrics.sh" "${kmersmash[@]}" "$genus"
+      ;;
+    mash)
+      bash "$script_dir/src/04.Mash_Metrics.sh" "${kmersani[@]}" "$genus" "${frag_lengths[@]}"
+      ;;
+    wraggling)
+      bash "$script_dir/src/05.wraggling.sh" "${kmersx[@]}" "${kmersy[@]}" "$my" "$mx"
+      ;;
+    graphs)
+      bash "$script_dir/src/06.Graphing.sh" "${kmersx[@]}" "${kmersy[@]}" "$my" "$mx"
+      ;;
+    boxplot)
+      bash "$script_dir/src/07.Summary_Feed.sh"
+      ;;
+    *)
+      echo "Unknown module: $module"
+      ;;
+  esac
 fi
 
-#Deactivate conda enviroments
+# Deactivate conda environment
 conda deactivate
+
